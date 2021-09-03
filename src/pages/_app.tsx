c71ed7f421c6ec8f5af2852d4701a7eb6af3919c
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-03 08:50:37 +0200

    removes SC relics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
-import { Normalize } from 'styled-normalize';
 import type { AppProps } from 'next/app';
 
-import GlobalStyles from '@/components/styles/GlobalStyles';
-import Typography from '@/components/styles/Typography';
 import Page from '@/components/Page';
 
 const App = ({ Component, pageProps }: AppProps) => (
     <>
-        <Normalize />
-        <GlobalStyles />
-        <Typography />
         <Page>
             <Component {...pageProps} />
         </Page>
