e2940bd4264d8da081763650fa65ebaf8c92a036
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-25 16:41:20 +0100

    ensure user enter email and name

--- src/components/GuestForm.tsx
+++ src/components/GuestForm.tsx
@@ -4,17 +4,30 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
+import validate from '@/../lib/validate';
 import FormFields from './FormFields';
+import { State } from './context';
 
 const GuestForm = () => {
+    const state = State();
     const router = useRouter();
+    const [error, setError] = useState<string>('');
+
+    const formSubmitHandler = () => {
+        const validationError = validate(state);
+        if (validationError) {
+            setError(validationError);
+        }
+        console.log('state: ', state);
+    };
 
     return (
         <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
             <Paper variant="elevation" elevation={1} sx={{ pt: '20px', pb: '20px', pl: 4, pr: 4, mt: '20px' }}>
                 <Typography component="h1" variant="h4" align="center">
-                    R.S.V.P
+                    {error || 'R.S.V.P'}
                 </Typography>
                 <>
                     <FormFields />
@@ -26,7 +39,7 @@ const GuestForm = () => {
                         >
                             Back
                         </Button>
-                        <Button variant="contained" sx={{ mt: 3, ml: 1, color: 'white' }}>
+                        <Button onClick={formSubmitHandler} variant="contained" sx={{ mt: 3, ml: 1, color: 'white' }}>
                             Send
                         </Button>
                     </Box>
