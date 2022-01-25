d76facd5d7350981c1f4bd741fc9d31373ac8988
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-25 14:02:54 +0100

    all form input events received except the slider

--- src/components/FormFields.tsx
+++ src/components/FormFields.tsx
@@ -7,89 +7,116 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Slider from '@mui/material/Slider';
 
-const AddressForm = () => (
-    <Grid container spacing={2}>
-        <Grid item xs={12} sm={12}>
-            <TextField required id="fullName" name="fullName" label="Full name" fullWidth variant="standard" />
-        </Grid>
-        <Grid item xs={12}>
-            <TextField required id="email" name="email" label="Email Address" fullWidth variant="standard" />
-        </Grid>
-        <Grid item xs={12} sm={12}>
-            <FormControl sx={{ transform: 'translateY(15px)' }}>
-                <FormLabel required id="demo-radio-buttons-group-label">
-                    Are you able to attend?
-                </FormLabel>
-                <RadioGroup
-                    aria-labelledby="rsvp-radio-buttons-group-label"
-                    defaultValue="yes"
-                    name="radio-buttons-group"
-                    sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
-                >
-                    <FormControlLabel value="yes" control={<Radio />} label="Yes, definitely" />
-                    <FormControlLabel value="no" control={<Radio />} label="No, I can't" />
-                    <FormControlLabel value="maybe" control={<Radio />} label="It's a maybe for now" />
-                </RadioGroup>
-            </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-            <FormControl sx={{ transform: 'translateY(15px)' }}>
-                <FormLabel required id="demo-radio-buttons-group-label">
-                    Are you bringing a partner or plus one?
-                </FormLabel>
-                <RadioGroup
-                    aria-labelledby="rsvp-radio-buttons-group-label"
-                    defaultValue="yes"
-                    name="radio-buttons-group"
-                    sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
-                >
-                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="no" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-        </Grid>
+const AddressForm = () => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.name, e.target.value);
+    };
 
-        <Grid item xs={12} sm={12}>
-            <TextField
-                id="partnerName"
-                name="partnerName"
-                label="If so, what is their name?"
-                fullWidth
-                variant="standard"
-            />
-        </Grid>
-        <Grid item xs={12} sm={12}>
-            <FormControl sx={{ transform: 'translateY(5px)' }}>
-                <FormLabel required id="demo-radio-buttons-group-label">
-                    Are you bringing children?
-                </FormLabel>
-                <RadioGroup
-                    aria-labelledby="rsvp-radio-buttons-group-label"
-                    defaultValue="yes"
-                    name="radio-buttons-group"
-                    sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
-                >
-                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="no" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-            <FormControl fullWidth>
-                <FormLabel id="demo-radio-buttons-group-label">If so how many?</FormLabel>
-                <Slider
-                    aria-label="Always visible"
-                    defaultValue={0}
-                    step={1}
-                    marks
-                    min={0}
-                    max={10}
-                    valueLabelDisplay="auto"
-                    sx={{ width: '98%', ml: '1%' }}
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+                <TextField
+                    onChange={handleChange}
+                    required
+                    id="fullName"
+                    name="fullName"
+                    label="Full name"
+                    fullWidth
+                    variant="standard"
                 />
-            </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    onChange={handleChange}
+                    required
+                    id="email"
+                    name="email"
+                    label="Email Address"
+                    fullWidth
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+                <FormControl sx={{ transform: 'translateY(15px)' }}>
+                    <FormLabel required id="demo-radio-buttons-group-label">
+                        Are you able to attend?
+                    </FormLabel>
+                    <RadioGroup
+                        onChange={handleChange}
+                        aria-labelledby="rsvp-radio-buttons-group-label"
+                        defaultValue="maybe"
+                        name="attending"
+                        sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
+                    >
+                        <FormControlLabel value="yes" control={<Radio />} label="Yes, definitely" />
+                        <FormControlLabel value="no" control={<Radio />} label="No, I can't" />
+                        <FormControlLabel value="maybe" control={<Radio />} label="It's a maybe for now" />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+                <FormControl sx={{ transform: 'translateY(15px)' }}>
+                    <FormLabel required id="demo-radio-buttons-group-label">
+                        Are you bringing a partner or plus one?
+                    </FormLabel>
+                    <RadioGroup
+                        onChange={handleChange}
+                        aria-labelledby="rsvp-radio-buttons-group-label"
+                        defaultValue={false}
+                        name="partner"
+                        sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
+                    >
+                        <FormControlLabel value control={<Radio />} label="Yes" />
+                        <FormControlLabel value={false} control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+                <TextField
+                    onChange={handleChange}
+                    id="partnerName"
+                    name="partnerName"
+                    label="If so, what is their name?"
+                    fullWidth
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+                <FormControl sx={{ transform: 'translateY(5px)' }}>
+                    <FormLabel required id="demo-radio-buttons-group-label">
+                        Are you bringing children?
+                    </FormLabel>
+                    <RadioGroup
+                        onChange={handleChange}
+                        aria-labelledby="rsvp-radio-buttons-group-label"
+                        defaultValue={false}
+                        name="children"
+                        sx={{ flexDirection: 'row', verticalAlign: 'bottom' }}
+                    >
+                        <FormControlLabel value control={<Radio />} label="Yes" />
+                        <FormControlLabel value={false} control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+                <FormControl fullWidth>
+                    <FormLabel id="demo-radio-buttons-group-label">If so how many?</FormLabel>
+                    <Slider
+                        // onChange={handleChange}
+                        aria-label="Always visible"
+                        defaultValue={0}
+                        step={1}
+                        marks
+                        min={0}
+                        max={10}
+                        valueLabelDisplay="auto"
+                        sx={{ width: '98%', ml: '1%' }}
+                    />
+                </FormControl>
+            </Grid>
         </Grid>
-    </Grid>
-);
+    );
+};
 
 export default AddressForm;
