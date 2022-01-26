bdcf4fa712842bf2b39eec344d0ca0c12c61624a
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-26 08:54:51 +0100

    successful insert into database returns feedback to client

--- src/pages/api/submit-guest.ts
+++ src/pages/api/submit-guest.ts
@@ -3,11 +3,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../../lib/mongodb';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log('req.body: ', req.body);
-
     const { db } = await connectToDatabase();
 
-    res.json({
-        thanks: 'you did it',
-    });
+    const { acknowledged } = await db.collection('rsvps').insertOne(req.body);
+
+    res.json({ success: acknowledged ? 'true' : 'false' });
 };
