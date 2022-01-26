c4076ad337d247b6dae1ae05c7f96051d7c090ab
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-26 11:44:04 +0100

    if error in validation, return error to client

--- src/pages/api/submit-guest.ts
+++ src/pages/api/submit-guest.ts
@@ -2,13 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 
 import { connectToDatabase } from '../../../lib/mongodb';
+import validateIncomingValues from '../../../lib/validateIncomingValues';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
 
     if (!session) return res.status(401);
 
-    validateIncomingValues();
+    const error = validateIncomingValues();
+
+    if (error) return res.status(400).json({ error });
 
     const { db } = await connectToDatabase();
 
