d1a305de049348ef701ba625e8b9731fc38006d0
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-26 09:53:46 +0100

    adds admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Admin: NextPage = () => {
+    console.log('Admin: ', Admin);
+    return (
+        <div>
+            <p>Admin page</p>
+        </div>
+    );
+};
+
+export default Admin;
