f2dccd9558e26f9437294d74e32c7246ec2c4164
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-27 08:38:11 +0100

    fixes emotion cache import error

--- src/lib/createEmotionCache.js
+++ src/lib/createEmotionCache.js
@@ -1,7 +1,9 @@
 const createCache = require('@emotion/cache');
 
+console.log('createCache: ', createCache.default);
+
 // prepend: true moves MUI styles to the top of the <head> so they're loaded first.
 // It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
-export default function createEmotionCache() {
-    return createCache({ key: 'css', prepend: true });
-}
+module.exports = function createEmotionCache() {
+    return createCache.default({ key: 'css', prepend: true });
+};
