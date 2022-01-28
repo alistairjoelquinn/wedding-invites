40596af784fa7639ecc1c564198caf6df67dc0a4
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-28 12:46:44 +0100

    adds debug logs to check password route

--- src/pages/api/check-password.ts
+++ src/pages/api/check-password.ts
@@ -2,7 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.body.password === process.env.ADMIN_PASSWORD) {
+        console.log('true');
         return res.json({ admin: true });
     }
+    console.log('false');
     return res.json({ admin: false });
 };
