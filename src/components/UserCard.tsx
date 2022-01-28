4721b74435da6e01954db212dc3798252ac4b79d
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-28 12:43:07 +0100

    reformats single user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -10,21 +10,27 @@ const UserCard = ({ guest }: Props) => (
     <Box>
         <Card variant="outlined" sx={{ p: 2, width: '60vw', mb: 4 }}>
             <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    {guest.fullName}
-                </Typography>
                 <Typography variant="h5" component="div">
+                    Name: {guest.fullName}
+                </Typography>
+                <Typography sx={{ mt: 1.5 }} color="text.secondary">
                     Attending: {guest.attending ? 'yes' : 'no'}
                 </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <Typography sx={{ mt: 1.5 }} color="text.secondary">
                     Bringing a partner: {guest.partner ? 'yes' : 'no'}
                 </Typography>
-                {guest.partnerName && <Typography variant="body2">Partner name: {guest.partnerName}</Typography>}
-                <Typography variant="h5" component="div">
+                {guest.partnerName && (
+                    <Typography sx={{ mt: 1.5 }} color="text.secondary">
+                        Partner name: {guest.partnerName}
+                    </Typography>
+                )}
+                <Typography sx={{ mt: 1.5 }} color="text.secondary">
                     Bringing children: {guest.children ? 'yes' : 'no'}
                 </Typography>
                 {guest.children && (
-                    <Typography variant="body2">Number of children: {guest.numberOfChildren}</Typography>
+                    <Typography sx={{ mt: 1.5 }} color="text.secondary">
+                        Number of children: {guest.numberOfChildren}
+                    </Typography>
                 )}
             </CardContent>
         </Card>
