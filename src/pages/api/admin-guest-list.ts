0f6c14426616be01c2c00086707ee851bfe4146c
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-28 10:38:10 +0100

    adds server side route auth check for secured routes

--- src/pages/api/admin-guest-list.ts
+++ src/pages/api/admin-guest-list.ts
@@ -1,13 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 import connectToDatabase from '@/lib/mongodb';
 
-export default async (_: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req });
+
+    if (!session) return res.status(401);
+
     const { db } = await connectToDatabase();
 
     const data = await db.collection('rsvps').find().toArray();
 
     console.log('data: ', data);
 
-    res.json(data);
+    return res.json(data);
 };
