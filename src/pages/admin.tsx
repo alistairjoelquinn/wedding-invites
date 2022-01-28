b100259e0a8c93a16f1981c0ca028be098474209
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-28 10:27:39 +0100

    admin panel can get full user guest list on button click

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next';
 
 const Admin: NextPage = () => {
-    console.log('Admin: ', Admin);
+    const getAdminGuestList = async () => {
+        const res = await fetch('/api/admin-guest-list');
+        const data = await res.json();
+        console.log('data: ', data);
+    };
+
     return (
         <div>
             <p>Admin page</p>
+            <button type="button" onClick={getAdminGuestList}>
+                Get Guest List
+            </button>
         </div>
     );
 };
