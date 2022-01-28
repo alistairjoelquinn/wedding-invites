aca7a769836c9243f7f562e7e76e9e0f7d9d0183
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-28 09:51:01 +0100

    adds email logic

--- src/lib/ses.js
+++ src/lib/ses.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const aws = require('aws-sdk');
 
 const ses = new aws.SES({
@@ -6,21 +7,54 @@ const ses = new aws.SES({
     region: 'eu-central-1',
 });
 
-exports.sendEmail = (recipient, message, subject) =>
+function checkResponse(rsvp) {
+    let response;
+    if (rsvp.attending === 'yes') {
+        response = `
+            You received another response to wedding! ${rsvp.fullName} is going to attend!
+
+            ${rsvp.partner ? `They are bringing a plus one called ${rsvp.partnerName}.` : `They are coming alone.`}
+
+            ${rsvp.children
+                ? `They are bringing ${rsvp.numberOfChildren} children.`
+                : `They are not bringing any children.`
+            }
+        `;
+    } else if (rsvp.attending === 'maybe') {
+        response = `
+            You received another response to wedding! ${rsvp.fullName} is a maybe for now.
+
+            ${rsvp.partner ? `If they do come they are bringing a plus one called ${rsvp.partnerName}.` : `If they do come they are coming alone.`}
+
+            ${rsvp.children
+                ? `If they do come they are bringing ${rsvp.numberOfChildren} children.`
+                : `If they do come they are not bringing any children.`
+            }
+        `;
+    } else if (rsvp.attending === 'no') {
+        response = `
+            You received another response to wedding. ${rsvp.fullName} is not able to attend.
+        `;
+    }
+    return response;
+}
+
+
+exports.sendEmail = (rsvp) =>
     ses
         .sendEmail({
             Source: `Wedding Invitation Response <${process.env.ADMIN_EMAIL}>`,
             Destination: {
-                ToAddresses: [recipient],
+                ToAddresses: [process.env.ADMIN_EMAIL],
             },
             Message: {
                 Body: {
                     Text: {
-                        Data: message,
+                        Data: checkResponse(rsvp),
                     },
                 },
                 Subject: {
-                    Data: subject,
+                    Data: `Wedding response from ${rsvp.fullName}`,
                 },
             },
         })
