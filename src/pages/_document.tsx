54e2a45c4b5c99e14cc5cf34b2cfe4495bdab011
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-02-04 10:04:23 +0100

    adds fonts and type declarations for all font formats

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,10 +11,6 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta name="theme-color" content={theme.palette.primary.main} />
                     <link rel="shortcut icon" href="/static/favicon.ico" />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                    />
                     {(this.props as any).emotionStyleTags}
                 </Head>
                 <body>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
+import RobotoLight from './fonts/RobotoMono-Light.ttf';
+import Seasons from './fonts/seasons.otf';
+
 // Create a theme instance.
 const theme = createTheme({
     palette: {
@@ -17,6 +20,19 @@ const theme = createTheme({
     typography: {
         button: {
             textTransform: 'none',
+        },
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+            @font-face {
+              font-family: 'RobotoLight';
+              font-style: normal;
+              font-display: swap;
+              font-weight: 400;
+              src: local('Raleway'), local('Raleway-Regular'), url(${RobotoLight}) format('true-type');
+            }
+          `,
         },
     },
 });
