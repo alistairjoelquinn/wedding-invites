56abb7e70326a895b194d55300c2478ba5d29c4f
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-02-08 09:17:06 +0100

    admin route checks session before checking password

--- TODO.md
+++
@@ -1,5 +0,0 @@
-## TODO
-
-- loading spinner on submit
-- session check on all api routes
-- complete 404 and admin pages
--- src/pages/api/check-password.ts
+++ src/pages/api/check-password.ts
@@ -1,6 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req });
+
+    if (!session) return res.status(401);
+
     if (req.body.password === process.env.ADMIN_PASSWORD) {
         console.log('true');
         return res.json({ admin: true });
