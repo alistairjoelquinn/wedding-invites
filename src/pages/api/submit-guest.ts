66e0e8f2aeac2862933634329764e852ff35435b
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-02-09 16:34:13 +0100

    adds log to ses route for debugging deploy errors

--- src/pages/api/submit-guest.ts
+++ src/pages/api/submit-guest.ts
@@ -6,19 +6,25 @@ import connectToDatabase from '@/lib/mongodb';
 import { sendEmail } from '@/lib/ses';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log('ROUTE HIT');
     const session = await getSession({ req });
 
     if (!session) return res.status(401);
+    console.log('SESSION is valid');
 
     const error = validateIncomingValues(req.body);
 
     if (error) return res.json({ error });
+    console.log('NO ERRORS in the incoming body');
 
     const { db } = await connectToDatabase();
+    console.log('DB is connected');
 
     const { acknowledged } = await db.collection('rsvps').insertOne(req.body);
+    console.log('DATA WAS insert into the DB');
 
     await sendEmail(req.body);
+    console.log('EMAIL was sent');
 
     return res.json({ success: acknowledged ? 'true' : 'false' });
 };
